85bba3bbb85c9b759a16d2ef15dc03387d74d989
Kris Rau <krau612@gmail.com>
2017-10-17 10:31:31 -0700

    Sort delegates by rank, name, forged, forging time, productivity, and approval

--- src/app/pages/delegate-monitor/delegates/delegates.component.ts
+++ src/app/pages/delegate-monitor/delegates/delegates.component.ts
@@ -12,6 +12,9 @@ export class DelegatesComponent implements OnInit {
   @Input() curName: string;
   @Input() active: boolean = true;
 
+  private sortColumn: string = 'rank';
+  private sortDirection: string = 'asc';
+
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -20,6 +23,56 @@ export class DelegatesComponent implements OnInit {
   goToAddress(event, id: string) {
     event.preventDefault();
     this.router.navigate(['/address', id]);
+  }
+
+  sortDelegates(event, column: string) {
+    if (event) event.preventDefault();
+
+    if (this.sortColumn === column) {
+      if (this.sortDirection === 'asc') this.sortDirection = 'desc';
+      else this.sortDirection = 'asc';
+    } else {
+      this.sortDirection = 'asc';
+    }
+    this.sortColumn = column;
+
+    if (column === 'rate') {
+      this.list.sort((a, b): number => {
+        if (a.rate < b.rate)      return this.sortDirection === 'asc' ? 1 : -1;
+        else if (a.rate > b.rate) return this.sortDirection === 'asc' ? -1 : 1;
+        return 0;
+      });
+    } else if (column === 'username') {
+      this.list.sort((a, b): number => {
+        if (a.username < b.username)      return this.sortDirection === 'asc' ? -1 : 1;
+        else if (a.username > b.username) return this.sortDirection === 'asc' ? 1 : -1;
+        return 0;
+      });
+    } else if (column === 'forged') {
+      this.list.sort((a, b): number => {
+        if (a.forged < b.forged)      return this.sortDirection === 'asc' ? -1 : 1;
+        else if (a.forged > b.forged) return this.sortDirection === 'asc' ? 1 : -1;
+        return 0;
+      });
+    } else if (column === 'forgingTime') {
+      this.list.sort((a, b): number => {
+        if (a.forgingTime < b.forgingTime)      return this.sortDirection === 'asc' ? -1 : 1;
+        else if (a.forgingTime > b.forgingTime) return this.sortDirection === 'asc' ? 1 : -1;
+        return 0;
+      });
+    } else if (column === 'productivity') {
+      this.list.sort((a, b): number => {
+        if (a.productivity < b.productivity)      return this.sortDirection === 'asc' ? -1 : 1;
+        else if (a.productivity > b.productivity) return this.sortDirection === 'asc' ? 1 : -1;
+        return 0;
+      });
+    } else if (column === 'approval') {
+      this.list.sort((a, b): number => {
+        if (a.approval < b.approval)      return this.sortDirection === 'asc' ? -1 : 1;
+        else if (a.approval > b.approval) return this.sortDirection === 'asc' ? 1 : -1;
+        return 0;
+      });
+    }
   }
 
 }
