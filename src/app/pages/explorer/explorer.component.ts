93fd977b89882b1595ac84d423924aaf26182bc6
Patrick Zenhäusern <pz@tenhouse.ch>
2018-02-04 14:21:56 +0100

    remember active tab in chart (localstorage)

--- src/app/pages/explorer/explorer.component.ts
+++ src/app/pages/explorer/explorer.component.ts
@@ -5,6 +5,7 @@ import { CoinmarketService } from '../../shared/services/coinmarket.service';
 import { initCurrency } from '../../shared/const/currency';
 import { ThemeService } from '../../shared/services/theme.service';
 import { Subscription } from 'rxjs/Subscription';
+import { LocalStorageService } from '../../shared/services/local-storage.service';
 
 @Component({
   selector: 'ark-explorer',
@@ -13,13 +14,15 @@ import { Subscription } from 'rxjs/Subscription';
   providers: [CoinmarketService]
 })
 export class ExplorerComponent implements OnInit, OnDestroy {
+  private readonly chartStorageKey = 'chartPeriod';
+
   public transactions: any[];
   public blocks: any[];
   public chart: any;
   public isChartVisible: boolean;
   public currency: string = initCurrency.name;
   public currencyRate: number = initCurrency.value;
-  public activeChartTab = 'day';
+  public activeChartTab;
   public showTransactionLoader = false;
   public showBlockLoader = false;
 
@@ -30,7 +33,8 @@ export class ExplorerComponent implements OnInit, OnDestroy {
   constructor(private _explorerService: ExplorerService,
               private _currencyService: CurrencyService,
               private _marketService: CoinmarketService,
-              private _themeService: ThemeService) {
+              private _themeService: ThemeService,
+              private _localStorageService: LocalStorageService) {
     this.subscription = _currencyService.currencyChosen$.subscribe(currency => {
       this.currency = currency.name;
       this.currencyRate = currency.value;
@@ -56,11 +60,13 @@ export class ExplorerComponent implements OnInit, OnDestroy {
       this.getLastBlocks();
     }, 60000);
 
+    this.activeChartTab = this._localStorageService.getValue(this.chartStorageKey) || 'day';
     this._marketService.build(this.activeChartTab);
   }
 
   updateChart(event) {
     this.activeChartTab = event.target.id;
+    this._localStorageService.setValue(this.chartStorageKey, this.activeChartTab);
     this._marketService.build(this.activeChartTab);
   }
 
