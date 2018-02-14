4085c7bdf2d1bd537408fa5ad65b24e6c1da7efc
Brian Faust <hello@brianfaust.me>
2018-02-14 09:42:27 +0200

    set timezone in tests

--- test/unit/specs/mixins/readable-timestamp-ago.spec.js
+++ test/unit/specs/mixins/readable-timestamp-ago.spec.js
@@ -1,7 +1,10 @@
+import moment from 'moment-timezone'
 import mixins from '@/mixins'
 
 describe('readable timestamp ago mixin', () => {
-  it.skip('should properly format the given data', () => {
+  it('should properly format the given data', () => {
+    moment.tz.setDefault('America/Los_Angeles')
+
     mixins.readableTimestampAgo(22231900).should.equal('2 months ago')
   })
 })
--- test/unit/specs/mixins/readable-timestamp.spec.js
+++ test/unit/specs/mixins/readable-timestamp.spec.js
@@ -1,7 +1,10 @@
+import moment from 'moment-timezone'
 import mixins from '@/mixins'
 
 describe('readable timestamp mixin', () => {
-  it.skip('should properly format the given data', () => {
+  it('should properly format the given data', () => {
+    moment.tz.setDefault('America/Los_Angeles')
+
     mixins.readableTimestamp(22231900).should.equal('03.12.2017 22:31:40')
   })
 })
