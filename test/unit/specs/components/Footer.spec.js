19581d4cf56d7349e81480ad637f43cd99cd2e85
Patrick Zenhäusern <pz@tenhouse.ch>
2018-03-08 21:25:37 +0100

    fix all unit tests
    problems we had:
    
    - readable-timestamp.spec.js compared time to locale date time (depending on where you live the test gave a different result)
    - readable-timestamp-ago.spec.js always used "date time now", obviously this changes when time passes and the test was therefore never correct
    - Footer.spec.js had problems because i18n and global variables were missing

--- test/unit/specs/components/Footer.spec.js
+++ test/unit/specs/components/Footer.spec.js
@@ -1,6 +1,19 @@
-import { mount } from '@vue/test-utils'
+import { shallow, createLocalVue } from '@vue/test-utils'
 import sinon from 'sinon'
 import Footer from '@/components/Footer'
+import VueI18n from 'vue-i18n'
+
+const localVue = createLocalVue()
+localVue.use(VueI18n)
+const i18n = new VueI18n({
+  locale: "en",
+  fallbackLocale: 'en',
+  messages: { 'en': {}},
+  silentTranslationWarn: true
+})
+
+global.GIT_VERSION = "43496685190e3e768c3f5b1bc322ff8b7ed4c696"
+global.GIT_DATE = "2018-01-01"
 
 describe('Footer', () => {
   it('uses the current year always', () => {
@@ -11,8 +24,12 @@ describe('Footer', () => {
       useFakeTimers: new Date(year, 1)
     })
 
-    mount(Footer).vm.year.should.eql(year.toString())
+    const cmp = shallow(Footer, {
+      localVue,
+      i18n
+    })
 
+    expect(cmp.vm.year).toEqual(year.toString())
     sandbox.restore()
   })
 })
--- test/unit/specs/mixins/readable-timestamp-ago.spec.js
+++ test/unit/specs/mixins/readable-timestamp-ago.spec.js
@@ -2,6 +2,6 @@ import mixins from '@/mixins'
 
 describe('readable timestamp ago mixin', () => {
   it('should properly format the given data', () => {
-    expect(mixins.readableTimestampAgo(22231900)).toEqual('2 months ago')
+    expect(mixins.readableTimestampAgo(22231900, 26231900)).toEqual('2 months ago')
   })
 })
