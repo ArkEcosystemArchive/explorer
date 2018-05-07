66b7804f433f49c3dcc89ba3fb33d83f7de7a7d7
ItsANameToo <itsanametoo@outlook.com>
2018-05-07 23:02:41 +0200

    Fix special transactions that were incorrectly colored as incoming

--- src/components/tables/mobile/Transactions.vue
+++ src/components/tables/mobile/Transactions.vue
@@ -30,7 +30,7 @@
         <div class="list-row-border-b">
           <div>{{ $t("Amount (token)", {token: networkToken()}) }}</div>
           <div>
-            <transaction-amount :transaction="transaction"></transaction-amount>
+            <transaction-amount :transaction="transaction" :type="transaction.type"></transaction-amount>
           </div>
         </div>
 
