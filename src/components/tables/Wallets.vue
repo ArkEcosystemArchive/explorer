de9faba20ce7f127d1e1bb09a562f1a2daadb9a4
Edgar Goetzendorff <goetzendorff@cip.ifi.lmu.de>
2018-05-08 13:17:49 +0200

    fixes sorting by rank in tables/Wallets.vue and centers rank columns

--- src/components/tables/Wallets.vue
+++ src/components/tables/Wallets.vue
@@ -1,7 +1,7 @@
 <template>
   <loader :data="wallets">
     <table-component :data="wallets" :show-filter="false" :show-caption="false" table-class="w-full">
-      <table-column :sortable="false" show="index" :label="$t('Rank')" header-class="left-header-start-cell" cell-class="left-start-cell w-24">
+      <table-column show="vueTableComponentInternalRowId" :label="$t('Rank')" header-class="base-header-start-cell text-center w-32" cell-class="base-start-cell text-center">
         <template slot-scope="row">
           {{ getRank(row.vueTableComponentInternalRowId) }}
         </template>
