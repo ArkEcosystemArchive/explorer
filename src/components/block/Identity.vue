be85fc550e8481c1e17de51eae41901a74a90f9e
dated <dated@users.noreply.github.com>
2018-05-08 11:00:24 +0200

    show previous / next buttons on small devices (#148)
    * show previous / next buttons on small devices
    
    * added media query

--- src/components/block/Identity.vue
+++ src/components/block/Identity.vue
@@ -1,17 +1,21 @@
 <template>
-  <section class="mb-5 bg-theme-feature-background xl:rounded-lg flex items-center px-10 py-8">
-    <img class="mr-6" src="@/assets/images/icons/block.svg" />
-    <div class="flex-auto">
-      <div class="text-grey mb-2">{{ $t("Block ID") }}</div>
-      <div class="text-xl text-white semibold">{{ block.id }}</div>
+  <section class="mb-5 bg-theme-feature-background xl:rounded-lg flex flex-col sm:flex-row items-center px-10 py-8">
+    <div class="flex items-center flex-auto w-full sm:w-auto mb-5 sm:mb-0">
+      <img class="mr-6" src="@/assets/images/icons/block.svg" />
+      <div>
+        <div class="text-grey mb-2">{{ $t("Block ID") }}</div>
+        <div class="text-xl text-white semibold">{{ block.id }}</div>
+      </div>
     </div>
-    <div class="hidden sm:block">
+    <div class="flex w-full sm:block sm:w-auto justify-between">
       <button @click="prevHandler" class="hover-button block-pager-button mr-5">
         <img src="@/assets/images/icons/caret-left.svg" />
         <span class="ml-2 hidden">{{ $t("Previous block") }}</span>
+        <span class="ml-2 sm:hidden">{{ $t("Previous") }}</span>
       </button>
       <button @click="nextHandler" class="hover-button block-pager-button">
         <span class="mr-2 hidden">{{ $t("Next block") }}</span>
+        <span class="mr-2 sm:hidden">{{ $t("Next") }}</span>
         <img src="@/assets/images/icons/caret-right.svg" />
       </button>
     </div>
@@ -38,7 +42,9 @@ export default {
 </script>
 
 <style>
-.hover-button:hover span {
-  display: inline-block;
+@media (min-width: 576px) {
+  .hover-button:hover span:first-of-type {
+    display: inline-block;
+  }
 }
 </style>
