7b7da3eac46c4fdca37bfd8d6963c4d59b4a7728
dated <dated@users.noreply.github.com>
2018-05-08 04:29:35 +0200

    fixes menu for md sized displays (#141)

--- src/components/header/menu/Mobile.vue
+++ src/components/header/menu/Mobile.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="menu-container w-full max-w-480px bg-table-row list-reset absolute pin-b pin-l py-5 block sm:hidden">
+  <ul class="menu-container w-full max-w-480px bg-table-row list-reset absolute pin-b pin-l py-5 block md:hidden">
     <li class="flex justify-center hover:bg-grey-light">
       <router-link :to="{ name: 'home' }" tag="div" class="cursor-pointer py-5 w-64 flex-none border-b border-theme-nav-border">{{ $t("Home") }}</router-link>
     </li>
