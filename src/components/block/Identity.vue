0511cfd8bf9b59d0171a1576e0e8545f86a54700
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2018-05-09 05:11:30 +0200

    Fix next / previous block buttons no longer showing on small screens (#156)

--- src/components/block/Identity.vue
+++ src/components/block/Identity.vue
@@ -1,17 +1,21 @@
 <template>
-  <section class="mb-5 bg-theme-feature-background xl:rounded-lg flex items-center px-10 py-8">
-    <img class="mr-6" src="@/assets/images/icons/block.svg" />
-    <div class="flex-auto">
-      <div class="text-grey mb-2">{{ $t("Block ID") }}</div>
-      <div class="text-xl text-white semibold">{{ block.id }}</div>
+  <section class="mb-5 bg-theme-feature-background xl:rounded-lg flex flex-col md:flex-row items-center px-10 py-8">
+    <div class="flex items-center flex-auto w-full md:w-auto mb-5 md:mb-0">
+      <img class="mr-6" src="@/assets/images/icons/block.svg" />
+      <div>
+        <div class="text-grey mb-2">{{ $t("Block ID") }}</div>
+        <div class="text-xl text-white semibold">{{ block.id }}</div>
+      </div>
     </div>
-    <div class="hidden sm:block">
+    <div class="flex w-full md:block md:w-auto justify-between">
       <button @click="prevHandler" class="block-pager-button mr-5">
         <img src="@/assets/images/icons/caret-left.svg" />
-        <span class="ml-2">{{ $t("Previous block") }}</span>
+        <span class="ml-2 hidden md:block inline-button">{{ $t("Previous block") }}</span>
+        <span class="ml-2 md:hidden">{{ $t("Previous") }}</span>
       </button>
       <button @click="nextHandler" class="block-pager-button">
-        <span class="mr-2">{{ $t("Next block") }}</span>
+        <span class="mr-2 hidden md:block inline-button">{{ $t("Next block") }}</span>
+        <span class="mr-2 md:hidden">{{ $t("Next") }}</span>
         <img src="@/assets/images/icons/caret-right.svg" />
       </button>
     </div>
@@ -38,8 +42,8 @@ export default {
 </script>
 
 <style>
-@media (min-width: 576px) {
-  .hover-button:hover span:first-of-type {
+@media (min-width: 768px) {
+  .inline-button {
     display: inline-block;
   }
 }
