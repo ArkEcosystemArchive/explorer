29d90854d86c245eb1384105fdff7b4865a69f8c
air1one <noterwann@gmail.com>
2018-05-17 07:56:37 +0000

    Limit to 2 decimals for wallet balance in wallet lists

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -106,10 +106,10 @@ const methods = {
       })
   },
 
-  readableCrypto(value, appendCurrency = true) {
+  readableCrypto(value, appendCurrency = true, decimals = 8) {
     if (typeof value != 'undefined') {
       value = (value /= Math.pow(10, 8)).toLocaleString(undefined, {
-        maximumFractionDigits: 8,
+        maximumFractionDigits: decimals,
       })
 
       return appendCurrency ? `${value} ${store.getters['network/symbol']}` : value
