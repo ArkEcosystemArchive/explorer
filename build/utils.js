2ce7178854854b617d0b1bee199eb563a07dd4c0
ItsANameToo <itsanametoo@outlook.com>
2018-05-25 19:11:59 +0200

    Moved required plugins to optimizations and removed extract-text in favour of mini-css-extract plugin

--- build/utils.js
+++ build/utils.js
@@ -1,7 +1,7 @@
 'use strict'
 const path = require('path')
 const config = require('../config')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const packageConfig = require('../package.json')
 
 exports.assetsPath = function (_path) {
@@ -29,7 +29,7 @@ exports.cssLoaders = function (options) {
     }
   }
 
-  // generate loader string to be used with extract text plugin
+  // generate loader string to be used with Mini Css Extract plugin
   function generateLoaders (loader, loaderOptions) {
     const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
 
@@ -45,10 +45,7 @@ exports.cssLoaders = function (options) {
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: 'vue-style-loader'
-      })
+      return [ MiniCssExtractPlugin.loader, 'css-loader' ]
     } else {
       return ['vue-style-loader'].concat(loaders)
     }
