d23e1c5e0a4cdbf723ab84236c53043639e4cfde
dated <dated@users.noreply.github.com>
2018-05-30 03:18:30 +0200

    adds 'truncateWhere' param to truncate() (#223)
    * adds 'truncateWhere' param to truncate()
    
    * truncates vendorField value from the right in transaction table

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -54,12 +54,28 @@ const methods = {
     return typeof compareTime !== 'undefined' ? momentTime.from(getTime(compareTime)) : momentTime.fromNow();
   },
 
-  truncate(value, length = 13) {
-    const odd = length % 2;
-    const truncationLength = Math.floor((length - 1) / 2);
-    return (value.length > length)
-      ? `${value.slice(0, truncationLength - odd)}...${value.slice(value.length - truncationLength + 1)}`
-      : value
+  truncate(value, length = 13, truncateWhere = 'middle') {
+    switch (truncateWhere) {
+      case 'left':
+        return (value.length > length)
+          ? `...${value.slice(value.length - length + 3)}`
+          : value
+
+      case 'middle':
+        const odd = length % 2;
+        const truncationLength = Math.floor((length - 1) / 2);
+        return (value.length > length)
+          ? `${value.slice(0, truncationLength - odd)}...${value.slice(value.length - truncationLength + 1)}`
+          : value
+
+      case 'right':
+        return (value.length > length)
+          ? `${value.slice(0, length - 3)}...`
+          : value
+
+      default:
+        return value
+    }
   },
 
   rawCurrency(value, currencyName) {
