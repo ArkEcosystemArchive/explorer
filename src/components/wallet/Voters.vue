fc4a60c4cd7f10d644027e88c51064a960856ad1
dated <dated@users.noreply.github.com>
2018-05-31 17:16:28 +0200

    hides 'See all' link if there are no voters (#243)

--- src/components/wallet/Voters.vue
+++ src/components/wallet/Voters.vue
@@ -2,8 +2,8 @@
   <div class="border-t list-row" v-show="Object.keys(voters).length">
     <div>{{ $t("Voters") }}</div>
     <div>
-      <span v-tooltip="{ content: $t('Only voters with more than 0.1 Ark'), placement: 'left' }" class="mr-2">{{ voters.length }}</span>
-      <router-link v-if="wallet.address" :to="{ name: 'wallet-voters', params: { address: wallet.address, username: username, page: 1 } }">{{ $t("See all") }}</router-link>
+      <span v-tooltip="{ content: $t('Only voters with more than 0.1 Ark'), placement: 'left' }" :class="voters.length ? 'mr-2' : ''">{{ voters.length }}</span>
+      <router-link v-if="wallet.address && voters.length" :to="{ name: 'wallet-voters', params: { address: wallet.address, username: username, page: 1 } }">{{ $t("See all") }}</router-link>
     </div>
   </div>
 </template>
