de02e2b376ac41f3217d74ff50656d2ba86ed584
ItsANameToo <itsanametoo@outlook.com>
2018-05-31 20:03:41 +0200

    Fixed lint issues

--- src/services/node.js
+++ src/services/node.js
@@ -5,10 +5,10 @@ class NodeService {
   async get(url, config) {
     const server = store.getters['network/server']
 
-    const response = await axios.get(`${server}/${url}`, config);
-    
+    const response = await axios.get(`${server}/${url}`, config)
+
     if (!response.data.success) { throw new Error('Error GET ' + url + ' : ' + JSON.stringify(response)) }
-    return response;
+    return response
   }
 }
 
