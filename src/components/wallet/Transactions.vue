ceb72f82eca85556376e231a9ee50ef45f59d376
dated <dated@users.noreply.github.com>
2018-05-31 05:11:53 +0200

    aligns h2 with h1 on block and wallet page (#232)

--- src/components/wallet/Transactions.vue
+++ src/components/wallet/Transactions.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="text-2xl mb-5 md:mb-6 px-10 sm:hidden text-theme-text-primary">{{ $t("Transactions") }}</h2>
+    <h2 class="text-2xl mb-5 md:mb-6 px-5 sm:hidden text-theme-text-primary">{{ $t("Transactions") }}</h2>
     <section class="page-section py-8">
       <nav class="mx-5 md:mx-10 mb-8 border-b flex items-end">
         <div
