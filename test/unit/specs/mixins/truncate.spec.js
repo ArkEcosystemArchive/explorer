606cb3652e6616ac04a07a6870862a5051f9d09d
ItsANameToo <itsanametoo@outlook.com>
2018-06-03 14:25:20 +0200

    Fix failing tests after merge

--- test/unit/specs/mixins/truncate.spec.js
+++ test/unit/specs/mixins/truncate.spec.js
@@ -3,21 +3,20 @@ import mixins from '@/mixins'
 describe('truncate mixin', () => {
 
   it('should properly truncate the given data, if given length is smaller than value', () => {
-    expect(mixins.truncate('Hello World', 1)).toEqual('...')
-    expect(mixins.truncate('Hello World', 10)).toEqual('Hell...orld')
-    expect(mixins.truncate('Hello Beautiful World', 12)).toEqual('Hello...World')
-  }),
+    expect(mixins.truncate('Hello World', 1)).toEqual('Hello Worl...')
+    expect(mixins.truncate('Hello World', 10)).toEqual('Hell...rld')
+    expect(mixins.truncate('Hello Beautiful World', 13)).toEqual('Hello...World')
+  })
 
   it('should properly truncate the given data if no length is given', () => {
     expect(mixins.truncate('Hello World')).toEqual('Hello World')
     expect(mixins.truncate('Hello Beatiful World')).toEqual('Hello...World')
-  }),
+  })
 
   it('should properly truncate the given data, when applicable', () => {
-    expect(mixins.truncate('Hello World', -1)).toEqual('Hello World')
-    expect(mixins.truncate('Hello World', 0)).toEqual('Hello World')
-    expect(mixins.truncate('Hello World', 5)).toEqual('He...ld')
+    expect(mixins.truncate('Hello World', 5)).toEqual('H...d')
     expect(mixins.truncate('Hello World', 100)).toEqual('Hello World')
+  })
 
   it('should properly format the given data', () => {
     expect(mixins.truncate('Hello World', 8)).toEqual('Hel...ld')
