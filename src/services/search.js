a5e5e90450e895fb80e1d877888a008b7157cab2
ItsANameToo <itsanametoo@outlook.com>
2018-06-13 19:50:38 +0200

    Remove redundant check in search functions

--- src/services/search.js
+++ src/services/search.js
@@ -3,27 +3,27 @@ import NodeService from '@/services/node'
 class SearchService {
   async findByAddress(address) {
     const response = await NodeService.get('accounts', {params: {address}})
-    return response.data.success ? response.data : false
+    return response.data
   }
 
   async findByUsername(username) {
     const response = await NodeService.get('delegates/get', {params: {username}})
-    return response.data.success ? response.data : false
+    return response.data
   }
 
   async findByPublicKey(publicKey) {
     const response = await NodeService.get('delegates/get', {params: {publicKey}})
-    return response.data.success ? response.data : false
+    return response.data
   }
 
   async findByBlockId(id) {
     const response = await NodeService.get('blocks/get', {params: {id}})
-    return response.data.success ? response.data : false
+    return response.data
   }
 
   async findByTransactionId(id) {
     const response = await NodeService.get('transactions/get', {params: {id}})
-    return response.data.success ? response.data : false
+    return response.data
   }
 }
 
