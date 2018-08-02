97b7f9d3feb227384c0f0f4f436e5b2f4598df79
Brian Faust <envoyer@pm.me>
2018-08-02 12:57:00 +0300

    fix: wrong total forged calculation
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/components/monitor/TotalForged.vue
+++ src/components/monitor/TotalForged.vue
@@ -27,7 +27,11 @@ export default {
     },
 
     getTotal() {
-      this.forged = (this.height - this.rewardOffset) * 2 * Math.pow(10, 8)
+      if(this.height < this.rewardOffset) {
+        this.forged = 0
+      } else {
+        this.forged = (this.height - this.rewardOffset) * 2 * Math.pow(10, 8)
+      }
     },
   },
 }
