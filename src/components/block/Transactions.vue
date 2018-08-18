5056465935091462a0ba33f60cab155695097c7a
dated <dated@users.noreply.github.com>
2018-08-18 15:31:57 +0200

    Fixes margins of 'Show more' wrapper element (#343)

--- src/components/block/Transactions.vue
+++ src/components/block/Transactions.vue
@@ -8,7 +8,7 @@
       <div class="sm:hidden">
         <table-transactions-mobile :transactions="transactions"></table-transactions-mobile>
       </div>
-      <div class="mx-10 mt-10 flex flex-wrap" v-if="transactions.length >= 25">
+      <div class="mx-5 sm:mx-10 mt-5 md:mt-10 flex flex-wrap" v-if="transactions.length >= 25">
         <router-link :to="{ name: 'block-transactions', params: { block: this.block.id, page: 2 } }" tag="button" class="show-more-button">
           {{ $t("Show more") }}
         </router-link>
