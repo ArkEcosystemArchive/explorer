ecae0b4315f1fb3deeb4fe8a701180dda03d4051
Edgar Goetzendorff <goetzendorff@cip.ifi.lmu.de>
2018-08-27 11:02:28 +0200

    Removes tooltip warnings in unit tests

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import moment from 'moment-timezone'
+import VTooltip from 'v-tooltip'
 
 Vue.config.productionTip = false
+Vue.use(VTooltip, { defaultHtml: false })
 
 moment.tz.setDefault('America/Los_Angeles')
