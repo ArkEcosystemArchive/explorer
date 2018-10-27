117863ed66757b081a3b80b5800338af445dadf8
brtastic <brtastic.dev@gmail.com>
2018-10-27 09:33:59 +0200

    Restore old padding values and disable language for mobile

--- src/components/header/ToggleCurrency.vue
+++ src/components/header/ToggleCurrency.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="$store.dispatch('ui/setHeaderType', isCurrencyMenu ? null : 'currencies')"
-    class="px-1 md:px-2 py-3 md:py-6 flex items-center text-sm border-b-2 margin-t-2 border-transparent hover:border-red">
+    class="px-2 md:px-4 py-3 md:py-6 flex items-center text-sm border-b-2 margin-t-2 border-transparent hover:border-red">
     <img class="md:mr-4 flex-none" :src="imageSource" style="height: 16px;" />
     <span class="whitespace-no-wrap text-theme-text-content hidden md:inline-block">
       ARK/{{ currencyName }}: {{ rawCurrency(currencyRate, currencyName) }}
