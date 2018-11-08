2f43bbc09464cfdd4046ee85182fb5ac3d27a02d
Edgar Goetzendorff <hello@dated.fun>
2018-11-08 17:52:35 +0100

    refactor: properly initialize and set locale (#450)
    * test: wait for table to be loaded before searching
    
    * refactor: properly initialize and set locale
    
    * refactor: hide language list on small screens

--- src/components/header/languages/Mobile.vue
+++ src/components/header/languages/Mobile.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="language-menu menu-container text-center max-w-480px justify-center bg-table-row list-reset absolute pin-b pin-r py-1 px-4 flex items-center block xl:hidden">
+  <ul class="language-menu menu-container text-center max-w-480px justify-center bg-table-row list-reset absolute pin-b pin-r py-1 px-4 items-center hidden md:flex xl:hidden">
     <li v-for="lang in languages"
             @click="setLanguage(lang)"
             :key="lang"
@@ -12,8 +12,8 @@
 </template>
 
 <script type="text/ecmascript-6">
-
 import { mapGetters } from 'vuex'
+import moment from 'moment'
 
 export default {
   computed: {
@@ -28,11 +28,20 @@ export default {
     setLanguage(language) {
       this.$store.dispatch('ui/setLanguage', language)
       this.setI18nLanguage(language)
+
+      const locale = language.split('-')[0]
+      this.$store.dispatch('ui/setLocale', locale)
+      this.setLocale(locale)
+
       this.$store.dispatch('ui/setHeaderType', null)
     },
 
     setI18nLanguage(language) {
       this.$i18n.locale = language
+    },
+
+    setLocale(locale) {
+      moment.locale(locale)
     },
 
     getLanguageFlag(language) {
