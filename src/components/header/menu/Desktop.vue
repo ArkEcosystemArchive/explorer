0354e5365b6d518787fe6d39fa49000244ffb940
Pedro-Souza <souza.vipedro@gmail.com>
2018-11-11 20:28:15 -0200

    adding close icon when menu is open

--- src/components/header/menu/Desktop.vue
+++ src/components/header/menu/Desktop.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="absolute pin px-4 md:px-8 hidden sm:flex bg-theme-nav-background xl:rounded-r-md">
-    <button
-      @click="closeMenu"
-      class='px-4 py-3 md:py-6 flex-none flex items-center border-b-2 margin-t-2 mr-3 border-transparent hover:border-red text-theme-text-secondary'>
-      <!-- Inline this SVG so we can change color dynamically -->
-      <svg
-        class="flex-none fill-current"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        width="15px" height="13px">
-        <path fill-rule="evenodd"  fill="currenColor" d="M13.499,3.000 L1.499,3.000 C0.671,3.000 -0.001,2.328 -0.001,1.500 C-0.001,0.671 0.671,-0.000 1.499,-0.000 L13.499,-0.000 C14.328,-0.000 14.999,0.671 14.999,1.500 C14.999,2.328 14.328,3.000 13.499,3.000 ZM1.499,5.000 L7.499,5.000 C8.328,5.000 9.000,5.671 9.000,6.500 C9.000,7.328 8.328,8.000 7.499,8.000 L1.499,8.000 C0.671,8.000 -0.001,7.328 -0.001,6.500 C-0.001,5.671 0.671,5.000 1.499,5.000 ZM1.499,10.000 L9.499,10.000 C10.328,10.000 11.000,10.671 11.000,11.500 C11.000,12.328 10.328,13.000 9.499,13.000 L1.499,13.000 C0.671,13.000 -0.001,12.328 -0.001,11.500 C-0.001,10.671 0.671,10.000 1.499,10.000 Z"/>
-      </svg>
+    <button @click="closeMenu" class="px-2 border-b-2 border-transparent hover:border-red">
+      <img src="@/assets/images/icons/cross.svg" />
     </button>
+    <span class="border-r mx-2 md:mx-4 lg:mx-6 my-4"></span>
 
     <router-link @click.native="closeMenu" :to="{ name: 'home' }" tag="button" class="menu-button">{{ $t("Home") }}</router-link>
     <router-link @click.native="closeMenu" :to="{ name: 'top-wallets', params: { page: 1 } }" tag="button" class="menu-button">{{ $t("Top Wallets") }}</router-link>
