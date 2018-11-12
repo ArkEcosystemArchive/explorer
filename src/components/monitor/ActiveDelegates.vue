9a7d3a4f32dec338f55c580e2715a5f27bef817a
Edgar Goetzendorff <hello@dated.fun>
2018-11-12 18:00:43 +0100

    fix: get translation only if there is a last block (#459)

--- src/components/monitor/ActiveDelegates.vue
+++ src/components/monitor/ActiveDelegates.vue
@@ -92,10 +92,11 @@ export default {
       }[row.forgingStatus.code]
 
       const lastBlock = row.forgingStatus.lastBlock
-      const translation = this.$i18n.t('Last block at height on', { height: lastBlock.height })
 
       return {
-        content: lastBlock ? `[${status}] ${translation} ${this.readableTimestamp(lastBlock.timestamp)}`
+        content: lastBlock ? `[${status}] ${
+          this.$i18n.t('Last block at height on', { height: lastBlock.height })
+        } ${this.readableTimestamp(lastBlock.timestamp)}`
           : status,
         classes: [`tooltip-bg-${row.forgingStatus.code}`, 'font-sans']
       }
