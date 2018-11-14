26951ab2c5ac2c7495194c70d61bc509cfb0e9ad
Edgar Goetzendorff <hello@dated.fun>
2018-11-14 23:23:41 +0100

    Update test/unit/specs/i18n/i18n.spec.js
    Co-Authored-By: Highjhacker <mc-somango@live.fr>

--- test/unit/specs/i18n/i18n.spec.js
+++ test/unit/specs/i18n/i18n.spec.js
@@ -10,6 +10,6 @@ describe('i18n', () => {
     expect(i18n.t('No results', 'nl')).toBe('Geen Resultaten')
     expect(i18n.t('No results', 'pt-br')).toBe('Sem resultados')
     expect(i18n.t('No results', 'pl')).toBe('Brak wyników')
-	expect(i18n.t('No results', 'fr')).toBe('Pas de résultats')
+    expect(i18n.t('No results', 'fr')).toBe('Pas de résultats')
   })
 })
