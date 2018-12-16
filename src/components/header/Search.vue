da273c70929d21fdde4c8077818ef236cc09ac1a
kalgoop <17824093+kalgoop@users.noreply.github.com>
2018-12-16 19:07:14 +0530

    Bug: fix disappearing closeSearch button

--- src/components/header/Search.vue
+++ src/components/header/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex items-center px-5 sm:px-10">
-    <button @click="$store.dispatch('ui/setHeaderType', null)">
+    <button @click="$store.dispatch('ui/setHeaderType', null)" style="flex-shrink: 0">
       <img src="@/assets/images/icons/cross.svg" />
     </button>
 
