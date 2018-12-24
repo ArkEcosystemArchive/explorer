f8f01fb3b7aaeedec4f7484717566798ca018ff0
highjhacker <mc-somango@live.fr>
2018-12-24 12:48:19 +0100

    Design done, almost finished, just need to display the name of the transaction type instead of his index

--- src/pages/Transactions.vue
+++ src/pages/Transactions.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="max-w-2xl mx-auto md:pt-5">
-      <div class="relative text-white z-20">
-        <select v-model="transactionType" @change="filterTransactions" class="absolute pin-r mt-px bg-white shadow rounded border overflow-hidden list-reset text-sm">
-          <option v-for="(item, index) in transactionsChoices" :value="index - 1">
-            {{ item }}
-          </option>
-        </select>
+    <section class="mb-5">
+      <div class="px-5 sm:px-10 py-8 bg-theme-feature-background flex xl:rounded-lg items-center justify-between">
+        <div class="mr-6 flex-none">
+          <img class="block" src="@/assets/images/icons/transaction.svg" />
+        </div>
+        <div class="flex-auto min-w-0">
+          <div class="text-grey mb-2">Transactions Type</div>
+          <div class="flex">
+            <div class="text-lg text-white semibold truncate">
+              <span class="mr-2">{{ transactionsChoices[transactionType+1]  || test }}</span>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col ml-4">
+          <div class="text-grey mb-2">{{ $t("Type") }}</div>
+          <div class="relative text-white z-20">
+            <span @click="selectOpen = !selectOpen" class="cursor-pointer flex items-center">
+              <span class="mr-1">{{ $t(transactionsChoices[transactionType + 1]) || test }}</span>
+              <svg :class="{ 'rotate-180': selectOpen }" class="fill-current" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                width="16px" height="16px">
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </span>
+            <ul v-show="selectOpen" class="absolute pin-r mt-px bg-white shadow rounded border overflow-hidden list-reset text-sm">
+              <li v-for="(txType, index) in transactionsChoices">
+              <div @click="filterTransactions(index - 1)" class="dropdown-button">{{ $t(txType) }}</div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
+    </section>
     <section class="page-section py-5 md:py-10">
       <div class="hidden sm:block">
         <table-transactions :transactions="transactions"></table-transactions>
@@ -28,22 +54,28 @@ export default {
     transactionsChoices: [
       'All', 'Transfer', 'Second Signature', 'Delegate Registration', 'Vote', 'MultiSignature'
     ],
-    transactionType: '',
-    currentPage: 2,
+    transactionType: -1,
+    currentPage: 0,
+    selectOpen: false,
+    test: localStorage.getItem('transactionType')
   }),
 
   created() {
+    this.transactionType = localStorage.getItem('transactionType') || -1
     this.$on('paginatorChanged', page => this.changePage(page))
   },
 
   async beforeRouteEnter (to, from, next) {
-    const response = await TransactionService.filterByType(to.params.page, localStorage.getItem('transactionType')) // TODO: set type based on what tx type we were looking for, might have to do this in the url as is done with transaction filtering on sent / received / all in a wallet
-    next(vm => vm.setTransactions(response))
+    const response = await TransactionService.filterByType(to.params.page, localStorage.getItem('transactionType'))
+    next(vm => {
+      vm.currentPage = to.params.page
+      vm.setTransactions(response)
+    })
   },
 
   async beforeRouteUpdate (to, from, next) {
     this.transactions = null
-    const response = await TransactionService.filterByType(to.params.page, localStorage.getItem('transactionType')) // TODO: set type based on what tx type we were looking for, might have to do this in the url as is done with transaction filtering on sent / received / all in a wallet
+    const response = await TransactionService.filterByType(to.params.page, localStorage.getItem('transactionType'))
     this.currentPage = to.params.page
     this.setTransactions(response)
     next()
@@ -59,11 +91,13 @@ export default {
       this.$router.push({ name: 'transactions', params: { page } })
     },
 
-    async filterTransactions() {
-      this.transactions = null // Used to show the loading indicator
-      const response = await TransactionService.filterByType(this.currentPage, this.transactionType) // TODO: fix hardcoded '1' parameter
+    async filterTransactions(index) {
+      this.transactions = null
+      this.selectOpen = false
+      this.transactionType = index
+      const response = await TransactionService.filterByType(this.currentPage, index)
       this.setTransactions(response)
-      localStorage.setItem('transactionType', this.transactionType)
+      localStorage.setItem('transactionType', index)
     }
   }
 }
