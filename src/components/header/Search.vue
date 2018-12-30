e575431f1fcf81821a06e902f4894d08474f98dd
Edgar Goetzendorff <hello@dated.fun>
2018-12-30 07:06:34 +0100

    misc: various ui tweaks (#527)
    * misc: vertically align close buttons
    
    * misc: add transition to address button
    
    * misc: reduce bottom padding on qr modal
    
    * misc: increase padding on close buttons

--- src/components/header/Search.vue
+++ src/components/header/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex items-center px-5 sm:px-10">
-    <button @click="$store.dispatch('ui/setHeaderType', null)" class="flex flex-none">
+    <button @click="$store.dispatch('ui/setHeaderType', null)" class="flex flex-none p-2">
       <img src="@/assets/images/icons/cross.svg" />
     </button>
 
