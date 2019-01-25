b839e155e4b653c8dd81ceabde371a76ef884fc0
Edgar Goetzendorff <hello@dated.fun>
2019-01-25 15:47:43 +0100

    refactor: disabled tab buttons if count is zero & fixes (#556)
    * misc: add 'voting for' translation string
    
    * refactor: helper methods in wallet transactions component
    
    * refactor: disabled tab buttons if count is zero
    
    * test: restore and combine wallet and delegate tests
    
    * fix: remove unnecessary api calls and fix tx count
    
    * test: call to useXpath()
    
    * test: wait for 'See all' anchor

--- src/components/wallet/Transactions.vue
+++ src/components/wallet/Transactions.vue
@@ -1,41 +1,67 @@
 <template>
   <div>
-    <h2 class="text-2xl mb-5 md:mb-6 px-5 sm:hidden text-theme-text-primary">{{ $t("Transactions") }}</h2>
+    <h2 class="text-2xl mb-5 md:mb-6 px-5 sm:hidden text-theme-text-primary">
+      {{ $t("Transactions") }}
+    </h2>
     <section class="page-section py-5 md:py-10">
       <nav class="mx-5 md:mx-10 mb-8 border-b flex items-end">
         <div
-          @click="type = 'all'"
+          @click="setType('all')"
           :class="[
-            type === 'all' ? 'text-2xl border-blue text-theme-text-primary' : 'text-lg text-theme-text-secondary border-transparent',
+            !isTypeSent && !isTypeReceived ? 'text-2xl border-blue text-theme-text-primary' : 'text-lg text-theme-text-secondary border-transparent',
             'mr-4 py-4 px-2 cursor-pointer border-b-3 hover:text-theme-primary hover:border-blue'
-          ]">
+          ]"
+        >
           {{ $t("All") }}
         </div>
         <div
-          @click="type = 'sent'"
+          @click="setType('sent')"
           :class="[
-            type === 'sent' ? 'text-2xl border-blue text-theme-text-primary' : 'text-lg text-theme-text-secondary border-transparent',
+            isTypeSent ? 'text-2xl border-blue text-theme-text-primary' : 'text-lg text-theme-text-secondary border-transparent',
+            !sentCount ? 'pointer-events-none text-theme-text-tertiary' : '',
             'mr-4 py-4 px-2 cursor-pointer border-b-3 hover:text-theme-text-primary hover:border-blue'
-          ]">
-          {{ $t("Sent") }} <span class="text-xs"> ( {{ sentCount }} ) </span>
+          ]"
+        >
+          {{ $t("Sent") }}
+          <span
+            :class="isTypeSent ? 'text-theme-text-secondary' : 'text-theme-text-tertiary'"
+            class="text-xs text-theme-text-secondary"
+          >
+            {{ sentCount }}
+          </span>
         </div>
         <div
-          @click="type = 'received'"
+          @click="setType('received')"
           :class="[
-            type === 'received' ? 'text-2xl border-blue text-theme-text-primary' : 'text-lg text-theme-text-secondary border-transparent',
+            isTypeReceived ? 'text-2xl border-blue text-theme-text-primary' : 'text-lg text-theme-text-secondary border-transparent',
+            !receivedCount ? 'pointer-events-none text-theme-text-tertiary' : '',
             'mr-4 py-4 px-2 cursor-pointer border-b-3 hover:text-theme-text-primary hover:border-blue'
-          ]">
-          {{ $t("Received") }} <span class="text-xs"> ( {{ receivedCount }} ) </span>
+          ]"
+        >
+          {{ $t("Received") }}
+          <span
+            :class="isTypeReceived ? 'text-theme-text-secondary' : 'text-theme-text-tertiary'"
+            class="text-xs"
+          >
+            {{ receivedCount }}
+          </span>
         </div>
       </nav>
       <div class="hidden sm:block">
-        <table-transactions-detail :transactions="transactions"></table-transactions-detail>
+        <table-transactions-detail :transactions="transactions" />
       </div>
       <div class="sm:hidden">
-        <table-transactions-detail-mobile :transactions="transactions"></table-transactions-detail-mobile>
+        <table-transactions-detail-mobile :transactions="transactions" />
       </div>
-      <div class="mx-5 sm:mx-10 mt-5 md:mt-10 flex flex-wrap" v-if="transactions && transactions.length >= 25">
-        <router-link :to="{ name: 'wallet-transactions', params: { address: this.wallet.address, type, page: 2 } }" tag="button" class="show-more-button">
+      <div 
+        v-if="transactions && transactions.length >= 25"
+        class="mx-5 sm:mx-10 mt-5 md:mt-10 flex flex-wrap"
+      >
+        <router-link
+          :to="{ name: 'wallet-transactions', params: { address: this.wallet.address, type, page: 2 } }"
+          tag="button"
+          class="show-more-button"
+        >
           {{ $t("Show more") }}
         </router-link>
       </div>
@@ -59,8 +85,18 @@ export default {
     transactions: null,
     type: 'all',
     receivedCount: 0,
-    sentCount: 0,
+    sentCount: 0
   }),
+
+  computed: {
+    isTypeSent() {
+      return this.type === 'sent'
+    },
+
+    isTypeReceived() {
+      return this.type === 'received'
+    }
+  },
 
   watch: {
     wallet() {
@@ -68,13 +104,7 @@ export default {
 
       this.getSentCount()
       this.getReceivedCount()
-    },
-    type() {
-      this.getTransactions()
-
-      this.getSentCount()
-      this.getReceivedCount()
-    },
+    }
   },
 
   methods: {
@@ -91,14 +121,28 @@ export default {
     },
 
     async getReceivedCount() {
-      const response = await TransactionService.receivedByAddressCount(this.wallet.address)
-      this.receivedCount = response
+      if (this.wallet && this.wallet.address) {
+        const response = await TransactionService.receivedByAddressCount(this.wallet.address)
+        this.receivedCount = response
+      } else {
+        this.receivedCount = 0
+      }
     },
 
     async getSentCount() {
-      const response = await TransactionService.sentByAddressCount(this.wallet.address)
-      this.sentCount = response
+      if (this.wallet && this.wallet.address) {
+        const response = await TransactionService.sentByAddressCount(this.wallet.address)
+        this.sentCount = response
+      } else {
+        this.sentCount = 0
+      }
     },
+
+    setType(type) {
+      this.type = type
+
+      this.getTransactions()
+    }
   },
 }
 </script>
