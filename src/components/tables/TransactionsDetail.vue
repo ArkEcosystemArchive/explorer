c273498fe3b4fa0fb2a4bb8b0715932c7e8c83ec
Edgar Goetzendorff <hello@dated.fun>
2019-02-15 17:30:53 +0100

    fix: transaction tables sorting

--- src/components/tables/TransactionsDetail.vue
+++ src/components/tables/TransactionsDetail.vue
@@ -1,31 +1,64 @@
 <template>
   <loader :data="transactions">
-    <table-component v-if="transactions && transactions.length > 0" :data="transactions" sort-by="timestamp" sort-order="desc" :show-filter="false" :show-caption="false" table-class="w-full">
-      <table-column show="id" :label="$t('ID')" header-class="left-header-start-cell" cell-class="left-start-cell">
+    <table-component
+      v-if="transactions && transactions.length > 0"
+      :data="transactions"
+      sort-by="timestamp.unix"
+      sort-order="desc"
+      :show-filter="false"
+      :show-caption="false"
+      table-class="w-full"
+    >
+      <table-column
+        show="id"
+        :label="$t('ID')"
+        header-class="left-header-start-cell"
+        cell-class="left-start-cell"
+      >
         <template slot-scope="row">
           <link-transaction :id="row.id" :smart-bridge="row.vendorField" :show-smart-bridge-icon="showSmartBridgeIcon"></link-transaction>
         </template>
       </table-column>
 
-      <table-column show="timestamp" :label="$t('Timestamp')" header-class="left-header-cell hidden lg:table-cell" cell-class="left-cell hidden lg:table-cell">
+      <table-column
+        show="timestamp.unix"
+        :label="$t('Timestamp')"
+        header-class="left-header-cell hidden lg:table-cell"
+        cell-class="left-cell hidden lg:table-cell"
+      >
         <template slot-scope="row">
           {{ readableTimestamp(row.timestamp.unix) }}
         </template>
       </table-column>
 
-      <table-column show="senderId" :label="$t('Sender')" header-class="left-header-cell" cell-class="left-cell">
+      <table-column
+        show="sender"
+        :label="$t('Sender')"
+        header-class="left-header-cell"
+        cell-class="left-cell"
+      >
         <template slot-scope="row">
           <link-wallet :address="row.sender"></link-wallet>
         </template>
       </table-column>
 
-      <table-column show="recipientId" :label="$t('Recipient')" header-class="left-header-cell" cell-class="left-cell">
+      <table-column
+        show="recipient"
+        :label="$t('Recipient')"
+        header-class="left-header-cell"
+        cell-class="left-cell"
+      >
         <template slot-scope="row">
           <link-wallet :address="row.recipient" :type="row.type" :asset="row.asset"></link-wallet>
         </template>
       </table-column>
 
-      <table-column show="amount" :label="$t('Amount (token)', { token: networkToken() })" header-class="right-header-cell" cell-class="right-cell">
+      <table-column
+        show="amount"
+        :label="$t('Amount (token)', { token: networkToken() })"
+        header-class="right-header-cell"
+        cell-class="right-cell"
+      >
         <template slot-scope="row">
           <span class="whitespace-no-wrap">
             <transaction-amount :transaction="row" :type="row.type"></transaction-amount>
@@ -33,7 +66,12 @@
         </template>
       </table-column>
 
-      <table-column show="fee" :label="$t('Fee (token)', { token: networkToken() })" header-class="right-header-cell hidden md:table-cell" cell-class="right-cell hidden md:table-cell">
+      <table-column
+        show="fee"
+        :label="$t('Fee (token)', { token: networkToken() })"
+        header-class="right-header-cell hidden md:table-cell"
+        cell-class="right-cell hidden md:table-cell"
+      >
         <template slot-scope="row">
           <span class="whitespace-no-wrap">
             {{ readableCrypto(row.fee) }}
@@ -41,10 +79,18 @@
         </template>
       </table-column>
 
-      <table-column show="confirmations" :label="$t('Confirmations')" header-class="right-header-end-cell" cell-class="right-end-cell">
+      <table-column
+        show="confirmations"
+        :label="$t('Confirmations')"
+        header-class="right-header-end-cell"
+        cell-class="right-end-cell"
+      >
         <template slot-scope="row">
           <div class="flex items-center justify-end whitespace-no-wrap">
-            <div v-if="row.confirmations <= activeDelegates" class="flex items-center justify-end whitespace-no-wrap">
+            <div
+              v-if="row.confirmations <= activeDelegates"
+              class="flex items-center justify-end whitespace-no-wrap"
+            >
               <span class="text-green inline-block mr-2">{{ row.confirmations }}</span>
               <img class="icon flex-none" src="@/assets/images/icons/clock.svg" />
             </div>
@@ -57,6 +103,7 @@
         </template>
       </table-column>
     </table-component>
+
     <div v-else class="px-5 md:px-10">
       <span>{{ $t("No results") }}</span>
     </div>
