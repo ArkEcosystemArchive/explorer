f54765d6defbc47eee80f7ea4568ef1f5405ad00
Marek van der Hoeven <info@marekhoeven.com>
2019-04-16 20:31:26 +0200

    fix: copy button on iOS (#609)

--- src/components/utils/Clipboard.vue
+++ src/components/utils/Clipboard.vue
@@ -60,7 +60,29 @@ export default {
         'position:absolute;top:0;left:0;z-index:-9999;opacity:0;'
 
       document.body.appendChild(textArea)
-      textArea.select()
+
+      const isiOSDevice = navigator.userAgent.match(/ipad|iphone/i)
+
+      if (isiOSDevice) {
+        const editable = textArea.contentEditable
+        const readOnly = textArea.readOnly
+
+        textArea.contentEditable = true
+        textArea.readOnly = false
+
+        const range = document.createRange()
+        range.selectNodeContents(textArea)
+
+        const selection = window.getSelection()
+        selection.removeAllRanges()
+        selection.addRange(range)
+
+        textArea.setSelectionRange(0, 999999)
+        textArea.contentEditable = editable
+        textArea.readOnly = readOnly
+      } else {
+        textArea.select()
+      }
 
       this.copying = true
       setTimeout(() => (this.copying = false), 1200)
