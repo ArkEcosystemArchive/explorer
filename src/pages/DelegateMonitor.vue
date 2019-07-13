09166ba73df4849e41dde54887adc11c82cef35a
Edgar Goetzendorff <hello@dated.fun>
2019-07-13 08:15:59 +0200

    fix: set correct margin and padding sizes

--- src/pages/DelegateMonitor.vue
+++ src/pages/DelegateMonitor.vue
@@ -5,7 +5,7 @@
     <MonitorHeader />
 
     <section class="page-section py-5 md:py-10">
-      <nav class="mx-8 sm:mx-10 mb-4 border-b flex items-end">
+      <nav class="mx-5 sm:mx-10 mb-4 border-b flex items-end">
         <div
           :class="activeTab === 'active' ? 'active-tab' : 'inactive-tab'"
           @click="activeTab = 'active'"
