0f0abca3449d3dadf357fa18eaa5e3b155d0b213
dated <hello@dated.fun>
2019-08-05 12:17:57 +0200

    refactor: add PaginationNavigationButton component

--- src/components/utils/pagination/Pagination.vue
+++ src/components/utils/pagination/Pagination.vue
@@ -1,45 +1,18 @@
 <template>
   <div class="Pagination mx-5 sm:mx-10 mt-5 md:mt-10">
-    <button
-      v-if="showFirst"
-      class="Pagination__Button--first pager-button mr-2"
+    <PaginationNavigationButton
+      :is-visible="showFirst"
+      type="first"
+      class="mr-2"
       @click="emitFirst"
-    >
-      <svg
-        class="inline"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        width="13px"
-        height="13px"
-      >
-        <path
-          fill-rule="evenodd"
-          fill="currentColor"
-          d="M8.1,6.5l3.7-4c0.3-0.3,0.3-0.9,0-1.3c-0.3-0.3-0.9-0.3-1.2,0L6.3,5.9c-0.3,0.3-0.3,0.9,0,1.2l4.3,4.6c0.2,0.2,0.4,0.3,0.6,0.3c0.2,0,0.4-0.1,0.6-0.3c0.3-0.3,0.3-0.9,0-1.3L8.1,6.5z M3.1,6.5l3.7-4c0.3-0.3,0.3-0.9,0-1.3c-0.3-0.3-0.9-0.3-1.2,0L1.3,5.9c-0.3,0.3-0.3,0.9,0,1.2l4.3,4.6C5.7,11.9,5.9,12,6.1,12c0.2,0,0.4-0.1,0.6-0.3c0.3-0.3,0.3-0.9,0-1.3L3.1,6.5z"
-        />
-      </svg>
-    </button>
+    />
 
-    <button
-      v-if="showPrevious"
-      class="Pagination__Button--previous pager-button mr-2"
+    <PaginationNavigationButton
+      :is-visible="showPrevious"
+      type="previous"
+      class="mr-2"
       @click="emitPrevious"
-    >
-      <svg
-        class="inline"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        width="13px"
-        height="13px"
-      >
-        <path
-          fill-rule="evenodd"
-          fill="currentColor"
-          d="M6.1,6.5l3.7-4c0.3-0.3,0.3-0.9,0-1.3c-0.3-0.3-0.9-0.3-1.2,0L4.3,5.9c-0.3,0.3-0.3,0.9,0,1.2l4.3,4.6C8.7,11.9,8.9,12,9.1,12c0.2,0,0.4-0.1,0.6-0.3c0.3-0.3,0.3-0.9,0-1.3L6.1,6.5z"
-        />
-      </svg>
-      <span class="ml-1 hidden lg:inline">{{ $t('PAGINATION.PREVIOUS') }}</span>
-    </button>
+    />
 
     <div
       v-click-outside="closePageInput"
@@ -91,56 +64,34 @@
       </div>
     </div>
 
-    <button
-      v-if="showNext"
-      class="Pagination__Button--next pager-button ml-2"
+    <PaginationNavigationButton
+      :is-visible="showNext"
+      type="next"
+      class="ml-2"
       @click="emitNext"
-    >
-      <span class="mr-1 hidden lg:inline">{{ $t('PAGINATION.NEXT') }}</span>
-      <svg
-        class="inline"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        width="13px"
-        height="13px"
-      >
-        <path
-          fill-rule="evenodd"
-          fill="currentColor"
-          d="M9.7,5.9L5.5,1.3c-0.3-0.3-0.9-0.3-1.2,0c-0.3,0.3-0.3,0.9,0,1.3l3.7,4l-3.7,4c-0.3,0.3-0.3,0.9,0,1.3C4.4,11.9,4.6,12,4.9,12c0.2,0,0.4-0.1,0.6-0.3l4.3-4.6C10.1,6.8,10.1,6.2,9.7,5.9z"
-        />
-      </svg>
-    </button>
+    />
 
-    <button
-      v-if="showLast"
-      class="Pagination__Button--last pager-button ml-2"
+    <PaginationNavigationButton
+      :is-visible="showLast"
+      type="last"
+      class="ml-2"
       @click="emitLast"
-    >
-      <svg
-        class="inline"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        width="13px"
-        height="13px"
-      >
-        <path
-          fill-rule="evenodd"
-          fill="currentColor"
-          d="M6.7,5.9L2.5,1.3c-0.3-0.3-0.9-0.3-1.2,0c-0.3,0.3-0.3,0.9,0,1.3l3.7,4l-3.7,4c-0.3,0.3-0.3,0.9,0,1.3C1.4,11.9,1.6,12,1.9,12c0.2,0,0.4-0.1,0.6-0.3l4.3-4.6C7.1,6.8,7.1,6.2,6.7,5.9z M11.7,5.9L7.5,1.3c-0.3-0.3-0.9-0.3-1.2,0s-0.3,0.9,0,1.3l3.7,4l-3.7,4c-0.3,0.3-0.3,0.9,0,1.3C6.4,11.9,6.6,12,6.9,12c0.2,0,0.4-0.1,0.6-0.3l4.3-4.6C12.1,6.8,12.1,6.2,11.7,5.9z"
-        />
-      </svg>
-    </button>
+    />
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { PaginationPageInput, PaginationSearchButton } from '@/components/utils/pagination'
+import {
+  PaginationNavigationButton,
+  PaginationPageInput,
+  PaginationSearchButton
+} from '@/components/utils/pagination'
 
 export default {
   name: 'Pagination',
 
   components: {
+    PaginationNavigationButton,
     PaginationPageInput,
     PaginationSearchButton
   },
