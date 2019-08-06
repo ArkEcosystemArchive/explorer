27dfe6d53dc03765c4f00b93a21cbbee9e5a021d
ItsANameToo <itsanametoo@protonmail.com>
2019-08-06 15:14:03 +0200

    fix: i18n being null in header

--- src/components/header/HeaderDefault.vue
+++ src/components/header/HeaderDefault.vue
@@ -119,6 +119,7 @@ export default {
 
   methods: {
     setMobilePlaceholder (showMobile) {
+      if (!this.$i18n) return
       this.placeholder = showMobile
         ? this.$i18n.t('SEARCH.PLACEHOLDER.SHORT')
         : this.$i18n.t('SEARCH.PLACEHOLDER.LONG')
