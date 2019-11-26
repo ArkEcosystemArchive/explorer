24baf9e128ef0e5f3ba7dca6a8450a5c363a3d53
ItsANameToo <itsanametoo@protonmail.com>
2019-11-26 16:53:57 +0100

    test: skip e2e test if table is not found (e.g. mainnet)

--- __tests__/e2e/specs/bridgechains_spec.js
+++ __tests__/e2e/specs/bridgechains_spec.js
@@ -6,63 +6,75 @@ describe("Bridgechains", () => {
     });
 
     it("should be possible to sort the table", () => {
-      cy.visit("/businesses/1");
+      cy.visit("/bridgechains/1");
 
-      cy.get("th")
-        .eq(1)
-        .as("creator")
-        .should("exist");
-      cy.get("@creator")
-        .invoke("text")
-        .should("include", "Creator");
+      cy.get('body').then(($body) => {
+        if ($body.find('table').length) {
+          cy.get("th")
+            .eq(1)
+            .as("creator")
+            .should("exist");
+          cy.get("@creator")
+            .invoke("text")
+            .should("include", "Creator");
 
-      cy.get("@creator").should("not.have.class", "sorting-asc");
-      cy.get("@creator").should("not.have.class", "sorting-desc");
+          cy.get("@creator").should("not.have.class", "sorting-asc");
+          cy.get("@creator").should("not.have.class", "sorting-desc");
 
-      cy.get("@creator").click();
-      cy.get("@creator").should("have.class", "sorting-asc");
+          cy.get("@creator").click();
+          cy.get("@creator").should("have.class", "sorting-asc");
 
-      cy.get("@creator").click();
-      cy.get("@creator").should("have.class", "sorting-desc");
+          cy.get("@creator").click();
+          cy.get("@creator").should("have.class", "sorting-desc");
+        }
+      });
     });
 
     it("should be possible to navigate to the next page and back", () => {
-      cy.get(".Pagination__Button--previous").should("not.exist");
-      cy.get(".Pagination__Button--next")
-        .should("exist")
-        .click();
+      cy.get('body').then(($body) => {
+        if ($body.find('table').length) {
+          cy.get(".Pagination__Button--previous").should("not.exist");
+          cy.get(".Pagination__Button--next")
+            .should("exist")
+            .click();
 
-      cy.url().should("include", "businesses/2");
+          cy.url().should("include", "bridgechains/2");
 
-      cy.get(".Pagination__Button--previous")
-        .should("exist")
-        .click();
+          cy.get(".Pagination__Button--previous")
+            .should("exist")
+            .click();
 
-      cy.url().should("include", "businesses/1");
+          cy.url().should("include", "bridgechains/1");
+        }
+      });
     });
 
     it("should be possible to click on a creator address", () => {
-      cy.visit("/businesses/1");
+      cy.visit("/bridgechains/1");
 
-      cy.get("h1")
-        .contains("Businesses")
-        .should("exist")
-        .then($heading => {
-          const heading = $heading.text();
+      cy.get('body').then(($body) => {
+        if ($body.find('table').length) {
+          cy.get("h1")
+            .contains("Bridgechains")
+            .should("exist")
+            .then($heading => {
+              const heading = $heading.text();
 
-          cy.get("tbody tr")
-            .first()
-            .within(() => {
-              cy.get("a")
+              cy.get("tbody tr")
                 .first()
-                .click();
+                .within(() => {
+                  cy.get("a")
+                    .first()
+                    .click();
+                });
+
+              cy.get("h1").should($heading2 => {
+                expect($heading2.text()).not.to.eq(heading);
+              });
+
+              cy.url().should("include", "wallets/");
             });
-
-          cy.get("h1").should($heading2 => {
-            expect($heading2.text()).not.to.eq(heading);
-          });
-
-          cy.url().should("include", "wallets/");
-        });
+        }
+      });
     });
   });
