f2d0c51acf54be220594adf719c1d7e54072db1e
ItsANameToo <itsanametoo@protonmail.com>
2019-12-11 13:45:35 +0100

    feat: show more button

--- src/pages/DelegateMonitor.vue
+++ src/pages/DelegateMonitor.vue
@@ -25,6 +25,12 @@
         :sort-query="sortParams[activeTab]"
         @on-sort-change="onSortChange"
       />
+
+      <div v-if="delegates && delegates.length === 51" class="mx-5 sm:mx-10 mt-5 md:mt-10 flex flex-wrap">
+        <RouterLink :to="{ name: activeTab === 'resigned' ? 'delegates-resigned' : 'delegates', params: { page: activeTab === 'standby' ? 5 : 3 } }" tag="button" class="button-lg">
+          {{ $t("PAGINATION.SHOW_MORE") }}
+        </RouterLink>
+      </div>
     </section>
   </div>
 </template>
