487c949bb73a785788bf756b10f89f9ed03502aa
Lemii <peter.spaargaren@gmail.com>
2020-01-10 14:32:13 +0100

    test: update test to use new response structure

--- __tests__/unit/specs/utils.spec.ts
+++ __tests__/unit/specs/utils.spec.ts
@@ -30,18 +30,24 @@ describe("Utils", () => {
       });
     });
 
-    it("should convert the input to an arktoshi amount", () => {
-      expect(inputProcessor("fee", "1")).toBe(100000000);
+    it("should convert the input to an arktoshi amount", async () => {
+      const { value } = await inputProcessor("fee", "1");
+      expect(value).toBe(100000000);
     });
 
-    it("should return the public key of a username", () => {
-      expect(inputProcessor("vote", "biz_classic")).toBe(
-        "020431436cf94f3c6a6ba566fe9e42678db8486590c732ca6c3803a10a86f50b92",
-      );
+    it("should return the public key of a username", async () => {
+      const { value } = await inputProcessor("vote", "biz_classic");
+      expect(value).toBe("020431436cf94f3c6a6ba566fe9e42678db8486590c732ca6c3803a10a86f50b92");
     });
 
-    it("should return the network timestamp of a date", () => {
-      expect(inputProcessor("timestamp-from", "2019-12-24T15:19:37.000Z")).toBe(87099577);
+    it("should return the network timestamp of a date", async () => {
+      const { value } = await inputProcessor("timestamp-from", "2019-12-24T15:19:37.000Z");
+      expect(value).toBe(87099577);
+    });
+
+    it("should return a timestamp", async () => {
+      const { ts } = await inputProcessor("fee", "1");
+      expect(typeof ts).toBe("number");
     });
   });
 });
