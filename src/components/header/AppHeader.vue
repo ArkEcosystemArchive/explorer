28c1486108ffe07c100c47b1fb2e5000969d4e0c
Edgar Goetzendorff <hello@dated.fun>
2020-01-14 11:07:16 +0100

    refactor: make menu components dry

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -12,7 +12,7 @@
       <HeaderCurrenciesDesktop v-else-if="headerType === 'currencies'" />
       <HeaderLanguagesDesktop v-else-if="headerType === 'languages'" />
       <HeaderDefault v-else />
-      <HeaderMenuDesktop v-if="menuVisible" />
+      <HeaderMenuDesktop v-if="menuVisible" :entries="menuEntries" />
     </div>
 
     <div class="w-full relative flex xl:hidden">
@@ -20,14 +20,14 @@
       <HeaderDefault v-else />
     </div>
 
-    <HeaderMenuMobile v-if="menuVisible" />
+    <HeaderMenuMobile v-if="menuVisible" :entries="menuEntries" />
     <HeaderCurrenciesMobile v-else-if="headerType === 'currencies'" />
     <HeaderLanguagesMobile v-else-if="headerType === 'languages'" />
   </header>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Provide, Vue } from "vue-property-decorator";
 import { mapGetters } from "vuex";
 import {
   HeaderDefault,
@@ -53,11 +53,37 @@ import {
   },
   computed: {
     ...mapGetters("ui", ["headerType", "menuVisible"]),
+    ...mapGetters("network", ["hasMagistrateEnabled"]),
+
+    menuEntries() {
+      const entries = [
+        { name: "home" },
+        { name: "top-wallets", params: { page: 1 } },
+        { name: "delegate-monitor" },
+      ]
+
+      if (this.hasMagistrateEnabled) {
+        entries.push(
+          { name: "bridgechains", params: { page: 1 } },
+          { name: "businesses", params: { page: 1 } }
+        );
+      }
+
+      entries.push({ name: "advanced-search", params: { page: 1 } });
+
+      return entries;
+    }
   },
 })
 export default class AppHeader extends Vue {
+  @Provide("normalizeName") public foo = this.normalizeName;
+
   private headerType: string;
   private menuVisible: boolean;
+
+  public normalizeName(name: string): string {
+    return name.replace("-", "_").toUpperCase();
+  }
 
   private closeHeader(): void {
     this.$store.dispatch("ui/setHeaderType", null);
