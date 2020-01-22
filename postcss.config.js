1fd3ef8047c10a8050b5285acf78c327fcdfebd9
ItsANameToo <itsanametoo@protonmail.com>
2020-01-22 13:36:42 +0100

    chore: update extractor and enable purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -6,15 +6,13 @@ module.exports = {
   plugins: [
     tailwindcss("./tailwind.config.js"),
     autoprefixer(),
-    false
+    true
       ? purgecss({
           content: ["./public/index.html", "./src/**/*.vue", "./src/**/*.js", "./src/**/*.ts"],
           extractors: [
             {
-              extractor: class TailwindExtractor {
-                static extract(content) {
-                  return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-                }
+              extractor: (content) => {
+                return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
               },
               extensions: ["html", "vue", "js", "ts"],
             },
