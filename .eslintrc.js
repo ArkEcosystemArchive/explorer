6d206ad6f10f5675fc4d8bd1d0b73b9b4c8f9825
highjhacker <mc-somango@live.fr>
2020-02-03 17:39:59 +0100

    chore: better rules for vue, now works like it should

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,68 +1,29 @@
 module.exports = {
   root: true,
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-    project: './tsconfig.json',
-    extraFileExtensions: ['.vue']
-  },
-
   env: {
-    browser: true,
-    node: true,
-    es6: true
+    node: true
   },
-
-  globals: {
-    GIT_DATE: true,
-    GIT_VERSION: true
-  },
-
-  plugins: ['@typescript-eslint', 'prettier', 'vue'],
+  plugins: ["@typescript-eslint", "prettier", "vue"],
 
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    //'plugin:prettier/recommended',
-    //'plugin:vue/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/recommended",
+    "@vue/typescript",
+    "@vue/prettier",
+    "prettier/@typescript-eslint"
   ],
-
   rules: {
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    //indent: ["error", 2],
-    quotes: ['error', 'double'],
-    "vue/html-quotes": ["error", "double"],
-    'prettier/prettier': 'error',
-
-    /*'vue/html-indent': [
-      'error',
-      'tab',
-      {
-        attribute: 1,
-        baseIndent: 1,
-        closeBracket: 0,
-        alignAttributesVertically: true,
-        ignores: []
-      }
-    ],
-    'vue/html-closing-bracket-newline': [
-      'error',
-      {
-        singleline: 'never',
-        multiline: 'always'
-      }
-    ],*/
-    'object-curly-spacing': ['error', 'always']
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+  },
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
   }
 };
