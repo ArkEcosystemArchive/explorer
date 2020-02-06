1d4864d811fbcd7b0d38e85fc77d73194241651d
highjhacker <mc-somango@live.fr>
2020-02-06 14:16:01 +0100

    chore: better eslint rules, github action integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,14 +14,21 @@ module.exports = {
     "prettier/@typescript-eslint"
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "no-case-declarations": "off",
+    "prefer-spread": "off",
+    "no-fallthrough": "off",
+    "getter-return": "off",
+    "comma-dangle": ["error", "always-multiline"],
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
