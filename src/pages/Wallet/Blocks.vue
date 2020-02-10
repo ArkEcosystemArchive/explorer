f1ffaec72c82c082a2d71fe1ee46b99aad0e8c5e
ItsANameToo <ItsANameToo@users.noreply.github.com>
2020-02-10 10:24:25 +0000

    style: resolve style guide violations

--- src/pages/Wallet/Blocks.vue
+++ src/pages/Wallet/Blocks.vue
@@ -32,12 +32,7 @@
       <div class="sm:hidden">
         <TableBlocksMobile :blocks="blocks" :hide-generated-by="true" />
       </div>
-      <Pagination
-        v-if="showPagination"
-        :meta="meta"
-        :current-page="currentPage"
-        @page-change="onPageChange"
-      />
+      <Pagination v-if="showPagination" :meta="meta" :current-page="currentPage" @page-change="onPageChange" />
     </section>
   </div>
 </template>
