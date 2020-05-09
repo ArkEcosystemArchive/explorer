c11866cceed574dba393adc3dd3eae1beb6ed1e7
brenopolanski <breno.polanski@gmail.com>
2020-05-09 15:51:23 -0300

    feat: create ListDividedItem component

---
+++ src/components/utils/listDivided/ListDividedItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <li
+    class="ListDividedItem flex flex-col py-4 w-full border-b border-dashed border-theme-line-separator"
+  >
+    <div
+      :class="isFloatingLabel ? 'flex-col items-start' : 'items-center'"
+      class="flex justify-between"
+    >
+      <span
+        :class="[
+          { 'font-semibold text-xs mb-1': isFloatingLabel },
+          itemLabelClass
+        ]"
+        class="ListDividedItem__label text-theme-page-text-light mr-5"
+      >
+        {{ label }}
+      </span>
+
+      <div
+        :class="itemValueClass"
+        class="ListDividedItem__value"
+      >
+        <slot>
+          <span
+            v-if="value"
+            class="text-theme-page-text"
+          >
+            {{ value }}
+          </span>
+        </slot>
+      </div>
+    </div>
+    <div
+      v-if="$slots.content"
+      class="mt-4"
+    >
+      <slot name="content" />
+    </div>
+  </li>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ListDividedItem extends Vue {
+  @Prop({ required: true }) public label!: string;
+  @Prop({ default: null }) public value!: string | number;
+  @Prop({ default: "" }) public itemLabelClass!: string;
+  @Prop({ default: "" }) public itemValueClass!: string;
+}
+</script>
