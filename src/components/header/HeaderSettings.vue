fd3badf5280edd3fadda99ae089130a51d62ef0a
brenopolanski <breno.polanski@gmail.com>
2020-05-15 16:18:47 -0300

    test: fix e2e tests

--- src/components/header/HeaderSettings.vue
+++ src/components/header/HeaderSettings.vue
@@ -3,9 +3,7 @@
     class="text-settings-icon px-3 md:px-2 py-3 md:py-4 flex flex-none items-center border-b-2 mt-2px border-transparent hover:border-theme-accents hover:text-blue transition cursor-pointer"
     @click="toggleModal"
   >
-    <button>
-      <SvgIcon name="filter" view-box="0 0 16 20" />
-    </button>
+    <SvgIcon name="filter" view-box="0 0 16 20" />
     <SettingsModal v-if="showModal" @close="toggleModal" />
   </div>
 </template>
